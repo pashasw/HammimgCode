b2b261febaab9dd55847b8fd9d484e3517aa345b
pashasw <pashasw@gmail.com>
2018-01-12 18:21:21 +0300

    first commit

---
+++ HammingCode/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace HammingCode
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.CipherTextBox = new System.Windows.Forms.TextBox();
+            this.CorrectCipherTextBox = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.MessageTextBox = new System.Windows.Forms.TextBox();
+            this.CodeMessageTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(31, 209);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(146, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Закодировать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // CipherTextBox
+            // 
+            this.CipherTextBox.Location = new System.Drawing.Point(278, 35);
+            this.CipherTextBox.Multiline = true;
+            this.CipherTextBox.Name = "CipherTextBox";
+            this.CipherTextBox.Size = new System.Drawing.Size(146, 52);
+            this.CipherTextBox.TabIndex = 1;
+            this.CipherTextBox.Text = "11110110001011110001";
+            // 
+            // CorrectCipherTextBox
+            // 
+            this.CorrectCipherTextBox.Location = new System.Drawing.Point(278, 111);
+            this.CorrectCipherTextBox.Multiline = true;
+            this.CorrectCipherTextBox.Name = "CorrectCipherTextBox";
+            this.CorrectCipherTextBox.Size = new System.Drawing.Size(146, 51);
+            this.CorrectCipherTextBox.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(278, 209);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(146, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Исправить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // MessageTextBox
+            // 
+            this.MessageTextBox.Location = new System.Drawing.Point(31, 35);
+            this.MessageTextBox.Multiline = true;
+            this.MessageTextBox.Name = "MessageTextBox";
+            this.MessageTextBox.Size = new System.Drawing.Size(146, 52);
+            this.MessageTextBox.TabIndex = 4;
+            this.MessageTextBox.Text = "11110110001011110001";
+            // 
+            // CodeMessageTextBox
+            // 
+            this.CodeMessageTextBox.Location = new System.Drawing.Point(31, 111);
+            this.CodeMessageTextBox.Multiline = true;
+            this.CodeMessageTextBox.Name = "CodeMessageTextBox";
+            this.CodeMessageTextBox.Size = new System.Drawing.Size(146, 51);
+            this.CodeMessageTextBox.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(464, 261);
+            this.Controls.Add(this.CodeMessageTextBox);
+            this.Controls.Add(this.MessageTextBox);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.CorrectCipherTextBox);
+            this.Controls.Add(this.CipherTextBox);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox CipherTextBox;
+        private System.Windows.Forms.TextBox CorrectCipherTextBox;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox MessageTextBox;
+        private System.Windows.Forms.TextBox CodeMessageTextBox;
+    }
+}
+
---
+++ HammingCode/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HammingCode
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string message = MessageTextBox.Text;
+            CodeMessageTextBox.Text = Coding.CodeWord(message);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string message = CipherTextBox.Text;
+            CorrectCipherTextBox.Text = Coding.CorrectionCode(message);
+        }
+    }
+    public class Coding
+    {
+        public static string InsertBit(string code)
+        {
+            int index = code.Length;
+            double degree = 0;
+            string newcode = code;
+            for (int i = 0; Math.Pow(2, i) < index; i++)
+            {
+                degree = Math.Pow(2, i);
+                newcode = newcode.Insert((int)degree - 1, "0");
+                index = index + 1;
+            }
+            return newcode;
+        }
+        public static int[,] CreateMatrix(int line, int column)
+        {
+            int[,] matrix = new int[line, column];
+            string output = "";
+            for (int i = 0; i < column; i++)
+            {
+                output = Convert.ToString(i+1, 2);
+                char[] arr = output.ToCharArray();
+                Array.Reverse(arr);
+                int j = 0;
+                for (; j < arr.Count(); j++)
+                {
+                    matrix[j, i] = Convert.ToInt32(arr[j] - '0');
+                }
+                for (; j < line; j++)
+                {
+                    matrix[j, i] = 0;
+                }
+            }
+            return matrix;
+        
+        }
+        public static string XOR(string R, string key)
+        {
+            string answer = "";
+            for (int i = 0; i < R.Length; i++)
+            {
+                if ((R[i] == '1' && key[i] == '1') || (R[i] == '0' && key[i] == '0'))
+                    answer += '0';
+                if ((R[i] == '0' && key[i] == '1') || (R[i] == '1' && key[i] == '0'))
+                    answer += '1';
+            }
+            return answer;
+        }
+
+        public static string CodeWord(string code)
+        {
+            string temp = "";
+            string newcode = "";
+            string insertstring = "";
+            string cipher = newcode;
+            newcode = InsertBit(code);
+            int[,] matrix = CreateMatrix(newcode.Length - code.Length, newcode.Length);
+            int sum = 0;
+            for (int i = 0; i < newcode.Length - code.Length; i++)
+            {
+                for (int j = 0; j < newcode.Length; j++)
+                {
+                    sum += ((Convert.ToInt32(newcode[j] - '0') ) * matrix[i,j]);
+                }
+                temp += sum % 2;
+                sum = 0;
+            }
+            int pow =0;
+            for (int i = 0; i < newcode.Length; i++)
+            {
+                if (i+1 == Math.Pow(2, pow))
+                { insertstring += temp[pow]; pow++; }
+                else insertstring += "0";
+            }
+            cipher = XOR(newcode,insertstring);
+                return cipher;
+        }
+        public static int Conv(string a)
+        {
+            int len = a.Length;
+            int nomber = 0;
+            int[] b = new int[len];
+            for (int i = 0; i < len; i++)
+            {
+                if (a[i] == '1')
+                    b[i] = 1;
+                else b[i] = 0;
+            }
+            for (int i = 0; i < len; i++)
+            {
+                nomber += b[i] * Convert.ToInt32(Math.Pow(2, len - i - 1));
+            }
+            return nomber;
+        }
+        public static string CorrectionCode(string cipher)
+        {
+            int powtu = 0;
+            while (Math.Pow(2, powtu) <= cipher.Length)
+                powtu++;
+            int[,] matrix = CreateMatrix(powtu, cipher.Length);
+            int sum = 0;
+            string temp = "";
+            for (int i = 0; i < powtu; i++)
+            {
+                for (int j = 0; j < cipher.Length; j++)
+                {
+                    sum += ((Convert.ToInt32(cipher[j] - '0')) * matrix[i, j]);
+                }
+                temp += sum % 2;
+                sum = 0;
+            }
+            char[] arr = temp.ToCharArray();
+            Array.Reverse(arr);
+            string s = "";
+            for (int i = 0; i < arr.Count(); i++)
+            {
+                s += arr[i];
+            }
+            int error = Conv(s);
+            char[] newcipher = cipher.ToCharArray();
+            if (newcipher[error - 1] == '0')
+                newcipher[error - 1] = '1';
+            else newcipher[error - 1] = '0';
+            string answer = "";
+            for (int i = 0; i < newcipher.Count(); i++)
+            {
+                answer += newcipher[i];
+            }
+            return answer;
+        }
+ 
+    }
+}
